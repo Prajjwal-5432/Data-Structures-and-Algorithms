303d0c9fbd90367b366da2979f1218d46bbeca4f
Prajjwal Sahu <prajjwalsahu5432@gmail.com>
2022-04-14 01:05:47 +0530

    Nested Segments

---
+++ Segment Tree/10.Nested Segments.cpp
@@ -0,0 +1,89 @@
+//Link: https://codeforces.com/edu/course/2/lesson/4/3/practice/contest/274545/problem/C
+
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<map>
+#include<set>
+#include<unordered_set>
+#include<algorithm>
+
+using namespace std;
+
+#define endl                   '\n'
+#define Max                    LLONG_MAX
+#define Min                    LLONG_MIN
+#define Mod                    1000000007
+#define remin(a, b)            a = a < b ? a : b
+#define remax(a, b)            a = a > b ? a : b
+#define nps                    string::npos
+#define int                    int64_t
+#define cont(it, s)            for(auto it = s.begin(); it != s.end(); ++it)
+#define rep(i, s, n)           for(int i = s; i < n; ++i)
+#define repr(i, n, s)          for(int i = n; i >= s; --i)
+#define read(a)                for(auto& xy: a) cin >> xy
+#define speak(a)               for(auto xy: a) cout << xy << ' '; cout << endl
+#define work(xy, a)            for(auto& xy: a)
+#define dbg(x)                 cerr << (#x) << " is " << (x) << endl
+#define all(xy)                (xy).begin(), (xy).end()
+#define rall(xy)               (xy).rbegin(), (xy).rend()
+vector<int> seg;
+
+void upd(int i, int x, int lx, int rx) {
+    if(lx == rx) {
+        seg[x] = 1;
+        return;
+    }
+
+    int mid = (lx + rx) / 2;
+
+    if(i <= mid)
+        upd(i, 2 * x + 1, lx, mid);
+    else
+        upd(i, 2 * x + 2, mid + 1, rx);
+
+    seg[x] = seg[2 * x + 1] + seg[2 * x + 2];
+}
+
+int query(int l, int r, int x, int lx, int rx) {
+    if(lx > r or rx < l) return 0;
+    if(lx >= l and rx <= r) return seg[x];
+
+    int mid = (lx + rx) / 2;
+    return query(l, r, 2 * x + 1, lx, mid) + query(l, r, 2 * x + 2, mid + 1, rx);
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("input.txt", "w", stdout);
+
+    int t = 1;
+//    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+
+        n <<= 1;
+
+        int x = 1;
+        while(x <= n) x <<= 1;
+
+        seg.resize(2 * x, 0);
+
+        vector<int> vis(n / 2 + 1, -1);
+
+        vector<int> ans(n / 2);
+        for(int i = 0, num; i < n; ++i) {
+            cin >> num;
+            if(vis[num] == -1) {
+                vis[num] = i;
+            } else {
+                ans[num - 1] = query(vis[num], i, 0, 0, x - 1);
+                upd(vis[num], 0, 0, x - 1);
+            }
+        }
+        speak(ans);
+    }
+}
