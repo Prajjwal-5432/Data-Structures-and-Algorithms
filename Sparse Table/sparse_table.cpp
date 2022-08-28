736cf2fc4c968c46e6fb5d60a7e5aa17a77c4c78
Prajjwal Sahu <prajjwalsahu5432@gmail.com>
2022-08-29 01:01:35 +0530

    Sparse Table

---
+++ Sparse Table/sparse_table.cpp
@@ -0,0 +1,35 @@
+struct sparse_table {
+    ll level, n;
+    vector<vector<ll>> table;
+    vector<ll> lg;
+    
+    sparse_table(vector<ll> &a) {
+        n = a.size();
+        level = ceil(log2(n));
+        table.resize(n, vector<ll> (level));
+        lg.resize(n + 1);
+        build(a);
+    }
+    
+    void build(vector<ll> &a) {
+        lg[1] = 0;
+        for(int i = 2; i < n + 1; ++i)
+            lg[i] = lg[i / 2] + 1;
+            
+        for(int i = 0; i < n; ++i) 
+            table[i][0] = a[i];
+        
+        for(int j = 1; j < level; ++j) {
+            for(int i = 0; i + (1LL << j) <= n; ++i) {
+                table[i][j] = min(table[i][j - 1], table[i + (1LL << (j - 1))][j - 1]);
+            }
+        }
+    }
+    
+    ll query(ll l, ll r) {
+        l--, r--;
+        ll j = lg[r - l + 1];
+    
+        return min(table[l][j], table[r - (1LL << j) + 1][j]);
+    }
+};
