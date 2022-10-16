aa928ff5c11da34e552d850e29f70ca55c0893e3
Prajjwal Sahu <prajjwalsahu5432@gmail.com>
2022-10-16 09:46:21 +0530

    Sparse Table

--- Sparse Table/sparse_table.cpp
+++ Sparse Table/sparse_table.cpp
@@ -1,35 +1,42 @@
 struct sparse_table {
-    ll level, n;
-    vector<vector<ll>> table;
-    vector<ll> lg;
+    int level, n;
+    vector<vector<int>> mx, mn;
+    vector<int> lg;
     
-    sparse_table(vector<ll> &a) {
+    sparse_table(vector<int> &a) {
         n = a.size();
         level = ceil(log2(n));
-        table.resize(n, vector<ll> (level + 1));
+        mx.resize(n, vector<int> (level + 1));
+        mn.resize(n, vector<int> (level + 1));
         lg.resize(n + 1);
         build(a);
     }
     
-    void build(vector<ll> &a) {
+    void build(vector<int> &a) {
         lg[1] = 0;
         for(int i = 2; i < n + 1; ++i)
             lg[i] = lg[i / 2] + 1;
-            
+        
         for(int i = 0; i < n; ++i) 
-            table[i][0] = a[i];
+            mn[i][0] = mx[i][0] = a[i];
         
         for(int j = 1; j <= level; ++j) {
             for(int i = 0; i + (1LL << j) <= n; ++i) {
-                table[i][j] = max(table[i][j - 1], table[i + (1LL << (j - 1))][j - 1]);
+                mx[i][j] = max(mx[i][j - 1], mx[i + (1LL << (j - 1))][j - 1]);
+                mn[i][j] = min(mn[i][j - 1], mn[i + (1LL << (j - 1))][j - 1]);
             }
         }
     }
     
-    ll query(ll l, ll r) {
+    int get_max(int l, int r) {
         l--, r--;
-        ll j = lg[r - l + 1];
+        int j = lg[r - l + 1];
+        return max(mx[l][j], mx[r - (1LL << j) + 1][j]);
+    }
     
-        return max(table[l][j], table[r - (1LL << j) + 1][j]);
+    int get_min(int l, int r) {
+        l--, r--;
+        int j = lg[r - l + 1];
+        return min(mn[l][j], mn[r - (1LL << j) + 1][j]);    
     }
 };
